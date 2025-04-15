86e06550e8daf9d930c8d1020db4b084132fb219
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-04-15 18:10:12 +0100

    finished up until ex 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num % 4 == 0 && num % 100 == 0 && num % 400 == 0) {
+        return true;
+    } else if (num % 4 != 0) {
+        return false;
+    } else if (num % 100 == 0) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let convertedTemperature = (temperature - 32) * (5/9);
+  if (convertedTemperature.toFixed(1) % 10 == 0) {
+    return convertedTemperature;
+  } else
+    return parseFloat(convertedTemperature.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let convertedTemperature = (temperature * 1.8) + 32;
+  if (convertedTemperature.toFixed(1) % 10 == 0) {
+    return convertedTemperature;
+  } else
+    return parseFloat(convertedTemperature.toFixed(1));
 };
 
 // Do not edit below this line
