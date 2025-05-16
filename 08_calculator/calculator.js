53e8c01c4bffafea78a1877d1aa9cdfe887407c7
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-05-16 21:42:49 +0100

    yes
    yes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  }else {
+    const sumallnums = arr.reduce((total, currentnum) => {
+      return total + currentnum;
+    })
+    return sumallnums;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0){
+    return 0;
+  } else {
+    const productAllNums = arr.reduce((total, currentItem) => {
+      return total * currentItem;
+    })
+    return productAllNums;
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1;
+  } else {
+    const arr = [num];
+    for (let numcpy = num -1; numcpy > 0; numcpy--){
+      arr.push(numcpy);
+    }
+    const factorial = arr.reduce((total, currentItem) => {
+      return total * currentItem;
+    })
+    return factorial;
+  }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const sentence = [...phrase].filter((char) => {
+        return /^[a-zA-Z0-9]*$/.test(char);
+    }).join('').toLowerCase();
+    let reverseSentence = sentence.split("").reverse().join("");
+    if (reverseSentence == sentence) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
