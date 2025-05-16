6068a01ee9f0579aa3a84e9d3cb84a5e9770ab63
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-05-16 23:03:12 +0100

    esz
    ezzz

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number == 0){
+        return 0;
+    }else if(number < 0){
+        return "OOPS";
+    }else if(number == 1){
+        return 1;
+    }else{
+        let n1 = 0, n2 = 1, nextNumber;
+        for (let i = 1; i < number; i++){
+            nextNumber = n1 + n2;
+            n1 = n2;
+            n2 = nextNumber;
+            console.log(`iterarion ${i}, fibonacci num = ${nextNumber}`);
+        }
+        return nextNumber;
+    }
 };
 
 // Do not edit below this line
