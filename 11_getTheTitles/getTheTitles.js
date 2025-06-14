4590529587f081ce35e49f269c0175f3e610959c
vrexitus <98414540+vrexitus@users.noreply.github.com>
2025-06-14 16:41:07 +0100

    done
    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const newArr = [];
+    console.log(books[0].title);
+    books.forEach(element => {
+        newArr.push(element.title);
+    });
+    return(newArr);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,37 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let elder = [];
+    let oldest = 0;
+    people.forEach(element => {
+        let age = 0;
+        if (element.yearOfDeath == null){
+            const currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+            let yearOfDeath = currentYear;
+            age = yearOfDeath - element.yearOfBirth;
+        } else {
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
+        if(oldest < age){
+            oldest = age;
+        }else{
+            oldest = oldest;
+        }
+    });
+    people.forEach(element => {
+        let age = 0;
+        if (element.yearOfDeath == null){
+            const currentDate = new Date();
+            let currentYear = currentDate.getFullYear();
+            let yearOfDeath = currentYear;
+            age = yearOfDeath - element.yearOfBirth;
+        } else {
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
+        if(age == oldest){
+            elder = element;
+        }
+    });
+    return elder;
 };
 
 // Do not edit below this line
